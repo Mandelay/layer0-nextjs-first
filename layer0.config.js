fa0f8064827b31927e3a98579c77a26fd19f35d7
Mandelay <mandelayster@gmail.com>
2022-07-19 16:53:16 +0300

    Update layer0.config.js

--- layer0.config.js
+++ layer0.config.js
@@ -3,6 +3,16 @@ module.exports = {
   backends: {
     // Define a domain or IP address to proxy as a backend
     // More on: https://docs.layer0.co/guides/layer0_config#backends
+    first: {
+      domainOrIp: 'ros-layer0-nextjs-first-default.layer0-limelight.link',
+      hostHeader: 'ros-layer0-nextjs-first-default.layer0-limelight.link',
+      disableCheckCert: true,
+    },
+    second: {
+      domainOrIp: 'ros-layer0-nextjs-second-default.layer0-limelight.link',
+      hostHeader: 'ros-layer0-nextjs-second-default.layer0-limelight.link',
+      disableCheckCert: true,
+    },
     api: {
       domainOrIp: 'layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link',
       hostHeader: 'layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link',
